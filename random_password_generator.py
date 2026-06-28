157cfb66b670220f48fdf2c30fb177706d33c097
Dannie <dannie.vision@gmail.com>
2026-06-28 07:40:36 +0530

    Delete random_password_generator.py

--- random_password_generator.py
+++
@@ -1,8 +0,0 @@
-def replace_nucleotide (dna_strand):
-    trna = dna_strand.replace('T','U')
-
-    return f'{trna}'
-
-# Enter your desired Nucleotide Strand Sequence inside ' ' below
-nucleotides = 'GATGGAACTTGACTACGTAAATT'
-print(replace_nucleotide(nucleotides))
